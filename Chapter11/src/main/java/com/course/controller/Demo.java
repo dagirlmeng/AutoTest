dd9b868d3e70eacf7ef97ec7466a5cee2d0a1c28
sxm <1172463162@qq.com>
2022-02-25 19:35:23 +0800

    mybatis实现insert

--- Chapter11/src/main/java/com/course/controller/Demo.java
+++ Chapter11/src/main/java/com/course/controller/Demo.java
@@ -1,10 +1,12 @@
 package com.course.controller;
 
+import com.course.model.TestUser;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "v1")
 public class Demo {
     //首先获取一个执行sql语句的对象,@Autowired启动及加载
+    //demo
     @Autowired
     private SqlSessionTemplate template;
     @RequestMapping(value = "getUserCount",method = RequestMethod.GET)
     @ApiOperation(value = "可以获取到用户数",httpMethod = "GET")
     public int getUserCount(){
         return template.selectOne("getUserCount");
+    }
+    /**
+     * 实现增删改查
+     */
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
+    public int addUser(@RequestBody TestUser testUser){
+        int result = template.insert("addUser",testUser);
+        return result;
     }
 }
 
---
+++ Chapter11/src/main/java/com/course/model/TestUser.java
@@ -0,0 +1,11 @@
+package com.course.model;
+
+import lombok.Data;
+
+@Data
+public class TestUser {
+    private int id;
+    private String name;
+    private int age;
+    private String sex;
+}
