e83c4f43248decef1b7348d46a83153d7e7bd709
sxm <1172463162@qq.com>
2022-02-28 21:09:03 +0800

    重写12章

--- Chapter12/src/main/java/com/course/cases/AddUserTest.java
+++ Chapter12/src/main/java/com/course/cases/AddUserTest.java
@@ -2,8 +2,16 @@ package com.course.cases;
 
 import com.course.config.TestConfig;
 import com.course.model.AddUserCase;
+import com.course.model.User;
 import com.course.utils.DataBaseUtil;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -15,5 +23,32 @@ public class AddUserTest {
         AddUserCase addUserCase = sqlSession.selectOne("addUserCase",1);
         System.out.println(addUserCase.toString());
         System.out.println(TestConfig.addUserUrl);
+        //发请求，获取结果
+        String result = getResult(addUserCase);
+        //验证返回结果
+        User user = sqlSession.selectOne("addUser",addUserCase);
+        System.out.println(user.toString());
+        Assert.assertEquals(addUserCase.getExpected(),result);
+    }
+
+    private String getResult(AddUserCase addUserCase) throws IOException {
+        HttpPost post = new HttpPost(TestConfig.addUserUrl);
+        JSONObject json = new JSONObject();
+        json.put("userName",addUserCase.getUserName());
+        json.put("password",addUserCase.getPassword());
+        json.put("sex",addUserCase.getSex());
+        json.put("age",addUserCase.getAge());
+        json.put("permission",addUserCase.getPermission());
+        json.put("isDelete",addUserCase.getIsDelete());
+        //设置头信息
+        post.setHeader("content-type","application/json");
+        StringEntity entity = new StringEntity(json.toString(),"utf-8");
+        //设置cookies
+        TestConfig.defaultHttpClient.setCookieStore( TestConfig.cookieStore);
+        //存放返回结果
+        String result;
+        HttpResponse response = TestConfig.defaultHttpClient.execute(post);
+        result= EntityUtils.toString(response.getEntity(),"utf-8");
+        return result;
     }
 }
--- Chapter12/src/main/java/com/course/config/TestConfig.java
+++ Chapter12/src/main/java/com/course/config/TestConfig.java
@@ -1,14 +1,20 @@
 package com.course.config;
 
-import org.apache.http.cookie.Cookie;
+import org.apache.http.client.CookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 public class TestConfig {
+    //登录接口uri
     public static String loginUrl;
+    //更新用户接口uri
     public static String updateUserInfoUrl;
+    //获取用户列表接口uri
     public static String getUserListUrl;
+    //获取用户接口uri
     public static String getUserInfoUrl;
+    //添加用户接口uri
     public static String addUserUrl;
+    //生命http客户端
     public static DefaultHttpClient defaultHttpClient;
-    public static Cookie cookie;
+    public static CookieStore cookieStore;
 }
