5a5114e6f199a9be9cab10b31df0a7f583bda32e
sxm <1172463162@qq.com>
2022-03-01 21:01:10 +0800

    获取列表

--- Chapter12/src/main/java/com/course/cases/GetUserInfoTest.java
+++ Chapter12/src/main/java/com/course/cases/GetUserInfoTest.java
@@ -2,11 +2,22 @@ package com.course.cases;
 
 import com.course.config.TestConfig;
 import com.course.model.GetUserInfoCase;
+import com.course.model.User;
 import com.course.utils.DataBaseUtil;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class GetUserInfoTest {
     @Test(dependsOnGroups = "loginTrue",description = "获取userid为1的用户信息")
@@ -15,5 +26,29 @@ public class GetUserInfoTest {
         GetUserInfoCase getUserInfoCase  = sqlSession.selectOne("getUserInfo",1);
         System.out.println(getUserInfoCase.toString());
         System.out.println(TestConfig.getUserInfoUrl);
+        //发送请求，获取结果
+        JSONArray result = getResult(getUserInfoCase);
+        User user = sqlSession.selectOne(getUserInfoCase.getExpected(),getUserInfoCase);
+        List userList = new ArrayList();
+        userList.add(user);
+        JSONArray jsonArray = new JSONArray(userList);
+        Assert.assertEquals(jsonArray,result);
+        //结果验证
+    }
+
+    private JSONArray getResult(GetUserInfoCase getUserInfoCase) throws IOException {
+        HttpPost post = new HttpPost(TestConfig.getUserInfoUrl);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id",getUserInfoCase.getId());
+        post.setHeader("content-type","application/json");
+        StringEntity entity = new StringEntity(jsonObject.toString(),"utf-8");
+        post.setEntity(entity);
+        TestConfig.defaultHttpClient.setCookieStore(TestConfig.cookieStore);
+        String result;
+        HttpResponse response = TestConfig.defaultHttpClient.execute(post);
+        result= EntityUtils.toString(response.getEntity(),"utf-8");
+        List resultList = Arrays.asList(result);
+        JSONArray array = new JSONArray(resultList);
+        return  array;
     }
 }
